a052968eb9e05315cf132fb53682555bec269c86
northes <me@northes.io>
2026-08-21 13:41:57 +0800

    perf: evaluate JSON workflows in a background worker

---
+++ frontend/src/utils/jsonWorkflow.worker.ts
@@ -0,0 +1,6 @@
+import {evaluateWorkflow,type WorkflowWorkerRequest,type WorkflowWorkerResponse} from '../components/JsonWorkflowEngine'
+
+type WorkerScope={onmessage:(event:MessageEvent<WorkflowWorkerRequest>)=>void;postMessage:(message:WorkflowWorkerResponse)=>void}
+const workerScope=self as unknown as WorkerScope
+
+workerScope.onmessage=event=>{const{ id,source,rules}=event.data;workerScope.postMessage({id,result:evaluateWorkflow(source,rules)})}
